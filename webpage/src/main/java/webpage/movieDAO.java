a1f937ef63a0f9cd9e900437f53d17e7312fa020
power933 <106124860+power933@users.noreply.github.com>
2022-09-08 18:07:25 +0900

    Add files via upload

---
+++ webpage/src/main/java/webpage/movieDAO.java
@@ -0,0 +1,14 @@
+package webpage;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class movieDAO {
+	private String arank;
+	private String arankpart;
+	private String person;
+	private String anm; 
+	private String aindate;
+}
